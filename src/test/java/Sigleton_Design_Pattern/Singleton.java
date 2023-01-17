c326053a47fb93c12fa4ed0d2aa1e7fac590072a
hasanucarim <wellgobaltrade@gmail.com>
2023-01-17 19:01:46 +0100

    first commit

---
+++ src/test/java/Sigleton_Design_Pattern/Singleton.java
@@ -0,0 +1,19 @@
+package Sigleton_Design_Pattern;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private static int count;
+    private String name;
+
+    private Singleton() {
+        count++;
+        name = "Singleton" + count;
+    }
+    public static Singleton getInstance() {
+        return singleton;
+    }
+    public void printName() {
+        System.out.println(name);
+    }
+}
---
+++ src/test/java/Sigleton_Design_Pattern/SingletonClient.java
@@ -0,0 +1,10 @@
+package Sigleton_Design_Pattern;
+
+public class SingletonClient {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            Singleton s = Singleton.getInstance();
+            s.printName();
+        }
+    }
+}
